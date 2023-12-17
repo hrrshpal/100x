04d2fd977f6c47c2b85862bc2c58488684b41ca0
harsh pal <hrrshpal@gmail.com>
2023-12-17 14:16:35 +0530

    basic js fundamentals

---
+++ week-1/code/1.js
@@ -0,0 +1,72 @@
+// let name = "jojo"
+// let age = 20
+// let isDead = true
+
+// console.log(name + "'s age is "+age)
+// if(isDead){
+//     console.log("and hes not dead yet")
+// } else{
+//     console.log("lol hes dead hehe")
+// }
+
+//loop
+// for(let i=0; i<10; i++){
+//     console.log("Jojo is a fkn retard")
+// }
+
+//array
+// let arr=[23,"kiki", 90,922]
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
+
+//objects
+// let obj = {
+//     name:"jojo",
+//     age:2,
+//     isRetard: true
+// }
+// console.log(obj.isRetard)
+
+//functions
+// function print(num){
+//     console.log("You passed "+num)
+// }
+// print(20)
+
+//callbacks
+// function calc (num1,num2,fn){
+//     fn(num1,num2)
+// }
+// function add(num1,num2){
+//     console.log(num1+num2)
+// }
+// function sub(num1,num2){
+//     console.log(num1-num2)
+// }
+// function mul(num1,num2){
+//     console.log(num1*num2)
+// }
+// function div(num1,num2){
+//     console.log(num1/num2)
+// }
+// function mod(num1,num2){
+//     console.log(num1%num2)
+// }
+
+// calc(2,3,add)
+// calc(4,23,sub)
+// calc(4,2,mul)
+// calc(4,2,div)
+// calc(4,3,mod)
+
+// setTimeout(function(){
+//     console.log("After 2 sec")
+// },2000)
+
+// let i=1
+// setInterval(function(){
+//     console.clear()
+//     console.log(i)
+//     i++;
+// },1000)
